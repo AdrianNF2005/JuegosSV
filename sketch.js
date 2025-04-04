c5aeeb5023f5dd643f96bb6f61b5e44943152b9f
AdrianNF <88874828+AdrianNF2005@users.noreply.github.com>
2025-04-05 00:32:31 +0200

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1015,7 +1015,7 @@ function draw() {
       image(QRImage, imgX, imgY, newW, newH);
 
       textSize(clampTextSize(width * 3));
-      text("Prueba en PC", width / 2, height*0.1);
+      text("Prueba en PC", width / 2, height*0.05);
       textSize(clampTextSize(width * 1.3));
       text("(Estos juegos se han creado específicamente", width / 2, imgY + newH / 2 + height * 0.11);
       text("para PC, introduce esta URL o escanea el código", width / 2, imgY + newH / 2 + height * 0.16);
